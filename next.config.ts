c9db58f7dfc031a974ab3e6dd7d125d62bb07860
solwerps@gmail.com <158217681+solwerps@users.noreply.github.com>
2026-02-08 17:20:58 -0600

    Fix Next build: remove esmExternals and disable turbo build

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // Necesario para poder usar paquetes ESM como @react-pdf/renderer con Webpack
-    esmExternals: "loose",
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
