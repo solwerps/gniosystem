32431bc82aa88c33d2418112b784e00d88884ba9
solwerps@gmail.com <158217681+solwerps@users.noreply.github.com>
2026-02-09 03:21:25 -0600

    Fix ESM externals for react-pdf on webpack build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    // Necesario para poder usar paquetes ESM como @react-pdf/renderer con Webpack
+    esmExternals: "loose",
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
